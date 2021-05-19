a54e07b548274a358126d9e425db722bcf25a4b3
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-05-19 11:13:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,8 +15,24 @@ function take_snapshot(){
 
 console.log('ml5 version : ',ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/UO--75_-d/model.json",modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Xdi91wvrn/model.json",modelLoaded);
 
 function modelLoaded(){
     console.log("Model Loaded!");
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_member_name").innerHTML=results[0].label;
+        document.getElementById("result_member_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
